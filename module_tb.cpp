e3e20931ca7d7c2407c010a722b84ead2335ecc5
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2018-09-25 20:11:27 +1000

    fix whitespace

--- module_tb.cpp
+++ module_tb.cpp
@@ -4,11 +4,11 @@
 #include <verilated_vcd_c.h>
 
 int main(int argc, char **argv) {
-	// Initialize Verilators variables
-	Verilated::commandArgs(argc, argv);
+    // Initialize Verilators variables
+    Verilated::commandArgs(argc, argv);
 
-	// Create an instance of our module under test
-	Vmodule_impl *tb = new Vmodule_impl;
+    // Create an instance of our module under test
+    Vmodule_impl *tb = new Vmodule_impl;
 
     Verilated::traceEverOn(true);
 
